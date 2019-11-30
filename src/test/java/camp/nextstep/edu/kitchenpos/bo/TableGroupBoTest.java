92357a2e7c457585a9cbe3ab6ed10f0377568798
Yun <>
2019-11-30 09:12:49 +0900

    test TableGroupBoTest - 테이블 그룹 삭제 테스트
    - 테이블 그룹 삭제 테스트 작성

--- src/test/java/camp/nextstep/edu/kitchenpos/bo/TableGroupBoTest.java
+++ src/test/java/camp/nextstep/edu/kitchenpos/bo/TableGroupBoTest.java
@@ -5,6 +5,8 @@ import static camp.nextstep.edu.kitchenpos.bo.MockBuilder.mockNotEmptyOrderTable
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import camp.nextstep.edu.kitchenpos.dao.OrderDao;
@@ -13,7 +15,9 @@ import camp.nextstep.edu.kitchenpos.dao.TableGroupDao;
 import camp.nextstep.edu.kitchenpos.model.OrderTable;
 import camp.nextstep.edu.kitchenpos.model.TableGroup;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -123,8 +127,54 @@ class TableGroupBoTest {
         );
     }
 
+    @DisplayName("특정 테이블 그룹을 삭제할 수 있다")
     @Test
     void delete() {
+        //given
+        Long tableGroupId = 100L;
+        List<OrderTable> savedOrderTables = Arrays.asList(
+            mockNotEmptyOrderTable(1L),
+            mockNotEmptyOrderTable(2L));
+        when(orderTableDao.findAllByTableGroupId(eq(tableGroupId))).thenReturn(savedOrderTables);
+
+        when(orderDao.existsByOrderTableIdAndOrderStatusIn(anyLong(), any()))
+            .thenReturn(false);
+        //when
+        tableGroupBo.delete(tableGroupId);
+
+        //then
+        assertThat(savedOrderTables).allMatch(x -> Objects.isNull(x.getTableGroupId()));
+
     }
 
+    @DisplayName("주문 테이블이 한 개도 없을 때 특정 테이블 그룹 삭제 실패")
+    @Test
+    void given_order_table_is_empty_then_delete_table_group_fails() {
+        //given
+        Long tableGroupId = 100L;
+        List<OrderTable> emptyOrderTables = Collections.emptyList();
+        when(orderTableDao.findAllByTableGroupId(eq(tableGroupId))).thenReturn(emptyOrderTables);
+
+        //then
+        assertThatIllegalArgumentException().isThrownBy(() ->
+            tableGroupBo.delete(tableGroupId)
+        );
+    }
+
+    @DisplayName("삭제하려는 테이블 그룹에 속하는 주문 테이블 중 ID가 가장 작은 주문테이블에 요리중/식사중인 주문이 있는 경우 특정 테이블 그룹 삭제 실패")
+    @Test
+    void given_any_order_table_is_not_completed_then_delete_table_group_fails() {
+        //given
+        Long tableGroupId = 100L;
+        OrderTable queriedFirstOrderTable = mockNotEmptyOrderTable(1L);
+        List<OrderTable> savedOrderTables = Arrays.asList(queriedFirstOrderTable);
+        when(orderTableDao.findAllByTableGroupId(eq(tableGroupId))).thenReturn(savedOrderTables);
+
+        when(orderDao.existsByOrderTableIdAndOrderStatusIn(eq(queriedFirstOrderTable.getId()), any()))
+            .thenReturn(true);
+        //then
+        assertThatIllegalArgumentException().isThrownBy(() ->
+            tableGroupBo.delete(tableGroupId)
+        );
+    }
 }
