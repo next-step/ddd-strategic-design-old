0bd1fc1913467ae9e1e1a0f86350cfd8a680e061
ohtaeg <otk1090@naver.com>
2019-12-04 16:42:48 +0900

    test (kitchen pos) : “MenuGroupBo 테스트 코드 피드백 반영

--- src/test/java/camp/nextstep/edu/kitchenpos/bo/MenuGroupBoTest.java
+++ src/test/java/camp/nextstep/edu/kitchenpos/bo/MenuGroupBoTest.java
@@ -22,39 +22,32 @@ import static org.mockito.Mockito.mock;
 @DisplayName("메뉴 그룹 Business Object 테스트 클래스")
 @ExtendWith(MockitoExtension.class)
 class MenuGroupBoTest {
-
     @Mock
     private MenuGroup menuGroup;
 
     @Mock
     private MenuGroupDao menuGroupDao;
 
-    @InjectMocks
-    private MenuGroupBo menuGroupBo;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
-
-    @DisplayName("메뉴그룹은 메뉴 그룹 번호와 메뉴 그룹명 속성들을 가지고 있다.")
+    @DisplayName("메뉴 그룹은 메뉴 그룹 번호와 메뉴 그룹명 속성들을 가지고 있다.")
     @Test
     void hasProperties() {
         String menuGroupIdPropertyName = "id";
         String menuGroupNamePropertyName = "name";
 
-        assertThat(menuGroup).hasFieldOrProperty(menuGroupIdPropertyName);
-        assertThat(menuGroup).hasFieldOrProperty(menuGroupNamePropertyName);
+        assertAll(
+                () -> assertThat(menuGroup).hasFieldOrProperty(menuGroupIdPropertyName),
+                () -> assertThat(menuGroup).hasFieldOrProperty(menuGroupNamePropertyName)
+        );
     }
 
     @DisplayName("메뉴 그룹을 생성할 수 있다.")
     @Test
     void create() {
         // given
-        given(menuGroupDao.save(any())).willReturn(menuGroup);
+        given(menuGroupDao.save(menuGroup)).willReturn(menuGroup);
 
         // when
-        MenuGroup savedMenuGroup = menuGroupDao.save(any());
+        MenuGroup savedMenuGroup = menuGroupDao.save(menuGroup);
 
         // then
         assertThat(savedMenuGroup).isNotNull()
@@ -65,26 +58,33 @@ class MenuGroupBoTest {
     @Test
     void list() {
         // given
+        final int menuGroupSize = 3;
+        final int menuGroupFirsIndex = 0;
+        final Long DEFAULT_ID = 1L;
+        final String menuGroupName = "치킨";
+
         List<MenuGroup> menuGroups = mock(List.class);
         MenuGroup menuGroup =  new MenuGroup();
-        menuGroup.setId(1L);
-        menuGroup.setName("치킨");
+        menuGroup.setId(DEFAULT_ID);
+        menuGroup.setName(menuGroupName);
 
         menuGroups.add(menuGroup);
         menuGroups.add(new MenuGroup());
         menuGroups.add(new MenuGroup());
 
         given(menuGroupDao.findAll()).willReturn(menuGroups);
-        given(menuGroups.get(0)).willReturn(menuGroup);
-        given(menuGroups.size()).willReturn(3);
+        given(menuGroups.get(menuGroupFirsIndex)).willReturn(menuGroup);
+        given(menuGroups.size()).willReturn(menuGroupSize);
 
         // when
         List<MenuGroup> savedMenuGroups = menuGroupDao.findAll();
 
         // then
-        assertThat(savedMenuGroups).isNotNull();
-        assertThat(menuGroups.get(0).getId()).isEqualTo(1L);
-        assertThat(menuGroups.get(0).getName()).isEqualTo("치킨");
-        assertThat(savedMenuGroups.size()).isEqualTo(3);
+        assertAll(
+                () -> assertThat(savedMenuGroups).isNotNull(),
+                () -> assertThat(menuGroups.get(menuGroupFirsIndex).getId()).isEqualTo(DEFAULT_ID),
+                () -> assertThat(menuGroups.get(menuGroupFirsIndex).getName()).isEqualTo(menuGroupName),
+                () -> assertThat(savedMenuGroups.size()).isEqualTo(menuGroupSize)
+        );
     }
 }
