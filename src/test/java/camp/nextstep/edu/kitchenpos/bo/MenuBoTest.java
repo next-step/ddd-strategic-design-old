dd5956a72ddbb5af03446fc03178fecd6629e51f
ohtaeg <otk1090@naver.com>
2019-12-04 16:33:25 +0900

    test (kitchen pos) : MenuBo 테스트 코드 피드백 반영

--- src/test/java/camp/nextstep/edu/kitchenpos/bo/MenuBoTest.java
+++ src/test/java/camp/nextstep/edu/kitchenpos/bo/MenuBoTest.java
@@ -15,7 +15,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +22,9 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 @DisplayName("메뉴 Business Object 테스트 클래스")
@@ -47,6 +48,8 @@ class MenuBoTest {
     @InjectMocks
     private MenuBo menuBo;
 
+    private final Long DEFAULT_ID = 1L;
+
     @DisplayName("메뉴는 메뉴 번호, 메뉴명, 금액, 메뉴 상품 속성들을 가지고 있다.")
     @Test
     void hasProperties() {
@@ -56,26 +59,29 @@ class MenuBoTest {
         String menuGroupIdPropertyName = "menuGroupId";
         String menuProductsPropertyName = "menuProducts";
 
-        assertThat(menu).hasFieldOrProperty(menuIdPropertyName);
-        assertThat(menu).hasFieldOrProperty(menuNamePropertyName);
-        assertThat(menu).hasFieldOrProperty(menuPricePropertyName);
-        assertThat(menu).hasFieldOrProperty(menuGroupIdPropertyName);
-        assertThat(menu).hasFieldOrProperty(menuProductsPropertyName);
+        assertAll("has properties menu",
+                () -> assertThat(menu).hasFieldOrProperty(menuIdPropertyName),
+                () -> assertThat(menu).hasFieldOrProperty(menuNamePropertyName),
+                () -> assertThat(menu).hasFieldOrProperty(menuPricePropertyName),
+                () -> assertThat(menu).hasFieldOrProperty(menuGroupIdPropertyName),
+                () -> assertThat(menu).hasFieldOrProperty(menuProductsPropertyName)
+        );
     }
 
     @DisplayName("하나의 메뉴는 여러 메뉴 상품들을 가질 수 있다")
     @Test
     void hasMultiMenuItem() {
         // given
+        final int menuProductSize = 3;
         List<MenuProduct> menuProducts = mock(List.class);
-        given(menuProducts.size()).willReturn(3);
+        given(menuProducts.size()).willReturn(menuProductSize);
 
         // when
         int size = menuProducts.size();
 
         // then
         assertThat(menuProducts.size()).isNotZero()
-                                       .isEqualTo(3);
+                                       .isEqualTo(menuProductSize);
     }
 
     @DisplayName("[메뉴 생성] 메뉴의 금액은 0 원 이상이면 성공을 반환 한다.")
@@ -90,8 +96,10 @@ class MenuBoTest {
         BigDecimal menuPrice = menu.getPrice();
 
         // then
-        assertThat(menuPrice).isGreaterThanOrEqualTo(BigDecimal.ZERO);
-        verify(menu, atLeast(1)).getPrice();
+        assertAll(
+                () -> assertThat(menuPrice).isGreaterThanOrEqualTo(BigDecimal.ZERO),
+                () -> then(menu).should().getPrice()
+        );
     }
 
     @DisplayName("[메뉴 생성] 메뉴의 금액은 0 원 미만일 경우 예외를 발생한다.")
@@ -99,15 +107,15 @@ class MenuBoTest {
     void whenPriceIsLessThanZero_thenFail() {
         // given
         BigDecimal price = BigDecimal.valueOf(-1);
+        given(menu.getPrice()).willReturn(price);
 
-        // when
-        when(menu.getPrice()).thenReturn(price);
-
-        // then
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> menuBo.create(menu));
-        assertThat(menu.getPrice()).isLessThan(BigDecimal.ZERO);
-        verify(menu, atLeast(1)).getPrice();
+        // when then
+        assertAll(
+                () -> assertThatExceptionOfType(IllegalArgumentException.class)
+                        .isThrownBy(() -> menuBo.create(menu)),
+                () -> assertThat(menu.getPrice()).isLessThan(BigDecimal.ZERO),
+                () -> verify(menu, atLeastOnce()).getPrice()
+        );
     }
 
     @DisplayName("[메뉴 생성] 메뉴의 금액이 null일 경우 예외를 발생한다.")
@@ -120,19 +128,21 @@ class MenuBoTest {
         when(menu.getPrice()).thenReturn(price);
 
         // then
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> menuBo.create(menu));
-        verify(menu, atLeast(1)).getPrice();
+        assertAll(
+                () -> assertThatExceptionOfType(IllegalArgumentException.class)
+                        .isThrownBy(() -> menuBo.create(menu)),
+                () -> then(menu).should().getPrice()
+        );
     }
 
     @DisplayName("[메뉴 생성] 메뉴가 메뉴 그룹에 속해 있으면 성공을 반환 한다.")
     @Test
     void whenMenuExistInMenuGroup_thenSuccess() {
         // given
-        given(menuGroupDao.existsById(any())).willReturn(true);
+        given(menuGroupDao.existsById(DEFAULT_ID)).willReturn(true);
 
         // when
-        Boolean isExist = menuGroupDao.existsById(any());
+        Boolean isExist = menuGroupDao.existsById(DEFAULT_ID);
 
         // then
         assertThat(isExist).isTrue();
@@ -142,14 +152,12 @@ class MenuBoTest {
     @Test
     void whenMenuExistInMenuGroup_thenFail() {
         // given
-        given(menu.getPrice()).willReturn(BigDecimal.valueOf(1));
-        given(menu.getMenuGroupId()).willReturn(1L);
-        given(menuGroupDao.existsById(any())).willReturn(true);
+        BigDecimal price = BigDecimal.valueOf(1);
+        given(menu.getPrice()).willReturn(price);
+        given(menu.getMenuGroupId()).willReturn(DEFAULT_ID);
+        given(menuGroupDao.existsById(DEFAULT_ID)).willReturn(false);
 
-        // when
-        when(menuGroupDao.existsById(any())).thenReturn(false);
-
-        // then
+        // when then
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> menuBo.create(menu));
     }
@@ -158,25 +166,25 @@ class MenuBoTest {
     @Test
     void whenMenuProductExist_thenSuccess() {
         // given
-        given(productDao.findById(any())).willReturn(Optional.of(new Product()));
+        given(productDao.findById(DEFAULT_ID)).willReturn(Optional.of(new Product()));
 
         // when
-        Product product = productDao.findById(any())
+        Product product = productDao.findById(DEFAULT_ID)
                                     .get();
 
         // then
-        assertThat(productDao.findById(any())).isNotNull();
+        assertThat(productDao.findById(DEFAULT_ID)).isNotNull();
     }
 
     @DisplayName("[메뉴 생성] 메뉴상품이 존재하지 않으면 런타임 예외를 발생 한다.")
     @Test
     void isNotExistMenuProduct() {
         // when
-        when(productDao.findById(any())).thenReturn(Optional.empty());
+        when(productDao.findById(DEFAULT_ID)).thenReturn(Optional.empty());
 
         // then
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> productDao.findById(any())
+                .isThrownBy(() -> productDao.findById(DEFAULT_ID)
                                             .orElseThrow(IllegalArgumentException::new));
     }
 
@@ -184,35 +192,42 @@ class MenuBoTest {
     @Test
     void whenPriceMultiplyQuantityEqualsToSum_thenSuccess() {
         // given
+        final int thousand = 1000;
+        final int twoThousand = 2000;
+        final long quantity = 2L;
+
         Product product = mock(Product.class);
-        given(product.getPrice()).willReturn(BigDecimal.valueOf(1000));
+        given(product.getPrice()).willReturn(BigDecimal.valueOf(thousand));
 
         MenuProduct menuProduct = mock(MenuProduct.class);
-        given(menuProduct.getQuantity()).willReturn(2L);
+        given(menuProduct.getQuantity()).willReturn(quantity);
 
         // when
         BigDecimal sum = BigDecimal.ZERO;
         sum = sum.add(product.getPrice().multiply(BigDecimal.valueOf(menuProduct.getQuantity())));
 
         // then
-        assertThat(sum).isEqualTo(BigDecimal.valueOf(2000));
+        assertThat(sum).isEqualTo(BigDecimal.valueOf(twoThousand));
     }
 
     @DisplayName("[메뉴 생성] 메뉴 상품의 합계 금액은 메뉴 금액보다 클 수 없다.")
     @Test
     void whenMenuProductPriceSum_notGreaterThan_MenuPrice() {
         // given
-        given(menu.getPrice()).willReturn(BigDecimal.valueOf(2000));
+        final BigDecimal productPrice = BigDecimal.valueOf(1000);
+        final BigDecimal menuPrice = BigDecimal.valueOf(2000);
+        final long quantity = 2L;
 
         Product product = mock(Product.class);
-        given(product.getPrice()).willReturn(BigDecimal.valueOf(1000));
+        given(product.getPrice()).willReturn(productPrice);
+        given(menu.getPrice()).willReturn(menuPrice);
 
         MenuProduct menuProduct = mock(MenuProduct.class);
-        given(menuProduct.getQuantity()).willReturn(2L);
+        given(menuProduct.getQuantity()).willReturn(quantity);
 
         // when
-        BigDecimal price = menu.getPrice();
         BigDecimal sum = BigDecimal.ZERO;
+        BigDecimal price = menu.getPrice();
         sum = sum.add(product.getPrice().multiply(BigDecimal.valueOf(menuProduct.getQuantity())));
         int result = price.compareTo(sum);
 
@@ -228,64 +243,67 @@ class MenuBoTest {
 
         // when
         Menu savedMenu = menuDao.save(menu);
-        when(savedMenu.getId()).thenReturn(1L);
+        when(savedMenu.getId()).thenReturn(DEFAULT_ID);
 
         // then
         assertThat(savedMenu.getId()).isNotNull()
-                                     .isEqualTo(1L);
+                                     .isEqualTo(DEFAULT_ID);
     }
 
     @DisplayName("[메뉴 생성] 생성된 메뉴 번호로 메뉴 상품들이 생성이 되면 성공을 반환 한다.")
     @Test
     void whenMenuProductsAreCreated_withCreatedMenuNumber_thenSuccess() {
         // given
-        long menuId = 1L;
         MenuProduct menuProduct = mock(MenuProduct.class);
         MenuProduct otherMenuProduct = mock(MenuProduct.class);
 
-        given(menuProductDao.save(any())).willReturn(menuProduct);
-        given(menuProductDao.save(any())).willReturn(otherMenuProduct);
+        given(menuProductDao.save(menuProduct)).willReturn(menuProduct);
+        given(menuProductDao.save(otherMenuProduct)).willReturn(otherMenuProduct);
 
-        given(menuProductDao.save(any()).getMenuId()).willReturn(menuId);
-        given(menuProductDao.save(any()).getMenuId()).willReturn(menuId);
+        given(menuProductDao.save(menuProduct).getMenuId()).willReturn(DEFAULT_ID);
+        given(menuProductDao.save(otherMenuProduct).getMenuId()).willReturn(DEFAULT_ID);
 
         // when
-        long menuProductMenuId = menuProductDao.save(any())
+        long menuProductMenuId = menuProductDao.save(menuProduct)
                                                .getMenuId();
-        long otherMenuProductMenuId = menuProductDao.save(any())
+        long otherMenuProductMenuId = menuProductDao.save(otherMenuProduct)
                                                     .getMenuId();
 
         // then
-        assertThat(menuProductMenuId).isEqualTo(otherMenuProductMenuId);
-        assertThat(menuId).isEqualTo(menuProductMenuId);
-        assertThat(menuId).isEqualTo(otherMenuProductMenuId);
+        assertAll(
+                () -> assertThat(menuProductMenuId).isEqualTo(otherMenuProductMenuId),
+                () -> assertThat(DEFAULT_ID).isEqualTo(menuProductMenuId),
+                () -> assertThat(DEFAULT_ID).isEqualTo(otherMenuProductMenuId)
+        );
     }
 
     @DisplayName("[메뉴 생성] 메뉴를 생성할 수 있다.")
     @Test
     void createMenu() {
         // given - 메뉴 객체 setting
-        long id = 1L;
+        BigDecimal price = BigDecimal.valueOf(20000);
+        int quantity = 1;
+
         Menu mockMenu = new Menu();
-        mockMenu.setId(id);
-        mockMenu.setPrice(BigDecimal.valueOf(20000));
-        mockMenu.setMenuGroupId(id);
+        mockMenu.setId(DEFAULT_ID);
+        mockMenu.setMenuGroupId(DEFAULT_ID);
+        mockMenu.setPrice(price);
 
         MenuProduct menuProduct = new MenuProduct();
-        menuProduct.setProductId(id);
-        menuProduct.setQuantity(1);
+        menuProduct.setProductId(DEFAULT_ID);
+        menuProduct.setQuantity(quantity);
 
         List<MenuProduct> menuProducts = new ArrayList<>();
         menuProducts.add(menuProduct);
         mockMenu.setMenuProducts(menuProducts);
 
         Product product = new Product();
-        product.setId(id);
-        product.setPrice(BigDecimal.valueOf(20000));
+        product.setId(DEFAULT_ID);
+        product.setPrice(price);
 
-        given(menuGroupDao.existsById(any())).willReturn(true);
-        given(productDao.findById(any())).willReturn(Optional.of(product));
-        given(menuDao.save(any())).willReturn(mockMenu);
+        given(menuGroupDao.existsById(DEFAULT_ID)).willReturn(true);
+        given(productDao.findById(DEFAULT_ID)).willReturn(Optional.of(product));
+        given(menuDao.save(mockMenu)).willReturn(mockMenu);
 
         // when
         final Menu savedMenu = menuBo.create(mockMenu);
@@ -299,29 +317,31 @@ class MenuBoTest {
     @Test
     void whenMenuCanSelect_thenSuccess() {
         // given
+        final int menuProductSize = 2;
         List<Menu> menus = mock(List.class);
         given(menuDao.findAll()).willReturn(menus);
-        given(menus.size()).willReturn(2);
+        given(menus.size()).willReturn(menuProductSize);
 
         // when
         List<Menu> allMenu = menuDao.findAll();
 
         //then
-        assertThat(allMenu.size()).isEqualTo(2);
+        assertThat(allMenu.size()).isEqualTo(menuProductSize);
     }
 
     @DisplayName("[메뉴 조회] 각 메뉴의 메뉴 상품들을 조회할 수 있다.")
     @Test
     void whenMenuProductCanSelect_thenSuccess() {
         // given
+        final int menuProductSize = 2;
         List<MenuProduct> menuProducts = mock(List.class);
-        given(menuProductDao.findAllByMenuId(any())).willReturn(menuProducts);
-        given(menuProducts.size()).willReturn(2);
+        given(menuProductDao.findAllByMenuId(DEFAULT_ID)).willReturn(menuProducts);
+        given(menuProducts.size()).willReturn(menuProductSize);
 
         // when
-        List<MenuProduct> allMenuProducts = menuProductDao.findAllByMenuId(any());
+        List<MenuProduct> allMenuProducts = menuProductDao.findAllByMenuId(DEFAULT_ID);
 
         //then
-        assertThat(allMenuProducts.size()).isEqualTo(2);
+        assertThat(allMenuProducts.size()).isEqualTo(menuProductSize);
     }
 }
