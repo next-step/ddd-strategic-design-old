a9719fbf2dfd41660778fa3166b0fc8e5fcafbfa
Juyoung <juy5790@outlook.com>
2019-12-23 23:33:16 +0900

    refactor 피드백 반영

--- src/test/java/camp/nextstep/edu/kitchenpos/dao/InMemoryMenuProductDao.java
+++ src/test/java/camp/nextstep/edu/kitchenpos/dao/InMemoryMenuProductDao.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
-public class InMemoryMenuProductDao implements camp.nextstep.edu.kitchenpos.dao.MenuProductDao {
+public class InMemoryMenuProductDao implements MenuProductDao {
 
     private static final Logger log = LoggerFactory.getLogger(InMemoryMenuProductDao.class);
 
